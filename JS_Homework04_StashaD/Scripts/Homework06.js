649ec16b07c15ab6967a2d24af58b9dc9bc921a3
Stasha Damovska <75165799+StashaAja@users.noreply.github.com>
2021-01-19 15:21:41 +0100

    Update - Homework 04 Task 06

--- JS_Homework04_StashaD/Scripts/Homework06.js
+++ JS_Homework04_StashaD/Scripts/Homework06.js
@@ -1,37 +1,25 @@
-//Ne e reshena ne znam kako da se snajdam bash, podole objasnuvam shto sakav da postignam. Mi treba help! :)
-let FirstName = ['Vanja', 'Dunja', 'Irma', 'Jan', 'Zofija'];
+let firstName = ['Vanja', 'Dunja', 'Irma', 'Jan', 'Zofija'];
 
-let LastName = ['Ahrimenko', 'Zabjakin', 'Damovska', 'Amski', 'Furnadziska'];
+let lastName = ['Ahrimenko', 'Zabjakin', 'Damovska', 'Amski', 'Furnadziska'];
 
-function Rainbow(arrayone, arraytwo) {
+function Rainbow(arrayOne, arrayTwo) {
 
-   let FinalNames = [];
+   let finalNames = [];
 
-   let OneElement = " ";
+   let oneElement = " ";
 
-   let arrayLenght = arrayone.length === arraytwo.length
+   for (let i = 0; i < arrayOne.length; i++) {
 
+       oneElement = `${i} . ${arrayOne[i]} ${arrayTwo[i]}`;
 
-   for (let i = 0; i < arrayLenght; i++) {
-
-      let NumericValue = i + 1
-
-      OneElement = NumericValue + " " + arrayone[i] + " " + arraytwo[i]
-
-      console.log(OneElement); //Tuka dobivam string: 1 Vanja Ahrimenko
-
-      FinalNames.push(OneElement); // Tuka ja popolnuvam nizata so prv string ["1 Vanja Ahrimenko"]
+       finalNames.push(oneElement);
+       
    }
-  
-   return FinalNames;
+
+   return finalNames;
+   // console.log(finalNames);
 }
 
-console.log(Rainbow(FirstName, LastName)); 
-
-//Objasnuvanje: Sakav da kreiram string od sekoj element so ist index vo nov prazen string, taka i stignav. 
-// Sledno toj string go push-nav kako nov element vo praznata niza:
-//QUESTION: Ne znam kako da napravam brojach ili koj loop da go vmetnam i kade, za da bez prestan mi iskreira takvi stringovi i site mi gi pushne u novata niza posledovatelno. 
-//Mene vsuhnost mi zavrshuva na prviot element.
-
+console.log(Rainbow(firstName, lastName)); 
 
 
