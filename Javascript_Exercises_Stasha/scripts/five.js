6f8b78ff41b249715a814946b1325865b29a4942
Stasha Damovska <75165799+StashaAja@users.noreply.github.com>
2021-01-26 16:02:37 +0100

    Update five.js

--- Javascript_Exercises_Stasha/scripts/five.js
+++ Javascript_Exercises_Stasha/scripts/five.js
@@ -1,1 +1,31 @@
+let click = document.getElementById("click");
+let select = document.getElementsByClassName("select")[0];
 
+
+function addInputToSelect(user) {
+    let newOption = document.createElement("option");
+    newOption.innerText = `Your Entry: ${user}`;
+    select.appendChild(newOption);
+}
+
+function validateAnEntry() {
+    var user = document.getElementById("user").value;
+    console.log(user);
+    console.log(typeof (user));
+
+    if (user === "") {
+        return alert(`Please enter your text in the Input field!`)
+    }
+    //--------------------------------Prashanje: Shto tochno gresham kaj zakomentiranata logika?
+    // if(user.toString.length == 0){
+    //     return alert(`Please enter your text in the Input field!`)
+    // }    
+    //---------------------------------------------------------------                     
+    else {
+        addInputToSelect(user);
+    }
+}
+
+click.addEventListener("click", function () {
+    validateAnEntry();
+})
