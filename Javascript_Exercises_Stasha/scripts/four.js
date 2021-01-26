735c0ce9b059d3e2a273a893f6d3f234344c6467
Stasha Damovska <75165799+StashaAja@users.noreply.github.com>
2021-01-26 13:31:38 +0100

    Update four.js

--- Javascript_Exercises_Stasha/scripts/four.js
+++ Javascript_Exercises_Stasha/scripts/four.js
@@ -1,7 +1,19 @@
-let inputUser = document.getElementById("inputUser").value;
-let inputButton = document.getElementById("inputButton");
-let selectUser = document.getElementById("selectUser")[0];
+let arrayOfStrings = ["Ljubov", "Denes e ubav den!", "Srekja", "Aja", "Snegulka", "Denes e sonchev den!"];
 
+function stringLarger(array) {
+    for (a of array) {
+        let result = (`This string:${a} has more than 10 characters`)
+        let b = a.toString().length
+        if (b > 10) {
+            return result;
+        }
+
+    }
+
+}
+
+stringLarger(arrayOfStrings);
+console.log(stringLarger(arrayOfStrings));
 
 
 
