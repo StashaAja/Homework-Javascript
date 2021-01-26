5f23f0fdd6523130cb3cadc8bf036388e16f8e9f
Stasha Damovska <75165799+StashaAja@users.noreply.github.com>
2021-01-26 17:09:16 +0100

    Update six.js

--- Javascript_Exercises_Stasha/scripts/six.js
+++ Javascript_Exercises_Stasha/scripts/six.js
@@ -1,12 +1,31 @@
-let inputOne = document.getElementById("inputOne");
-let inputTwo = document.getElementById("inputTwo");
-let inputTwo = document.getElementById("inputTwo");
+let inputButton = document.getElementById("inputButton");
 let paragraph = document.getElementById("paragraph");
 
+function calculateArea(inputUno, inputDos) {
+    let a = parseFloat(inputUno);
+    let b = parseFloat(inputDos);
 
-inputButton.addEventListener("click",function(){
-    paragraph 
-    let area = inputOne*inputTwo;
-    return area;
+    let area = a * b;
+    paragraph.innerText = `The area is: ${area}`;
+}
+
+function calculatePerimeter(inputUno, inputDos) {
+    let a = parseFloat(inputUno);
+    let b = parseFloat(inputDos);
+
+    let perimeter = 2 * a + 2 * b;
+    paragraph.innerText = `The perimeter is: ${perimeter}`;
+}
+
+inputButton.addEventListener("click", function () {
+    let inputOne = document.getElementById("inputOne").value;
+    let inputTwo = document.getElementById("inputTwo").value;
+    calculateArea(inputOne, inputTwo);
+    paragraph.addEventListener("mouseover", function () {
+        calculatePerimeter(inputOne, inputTwo);
+        this.style.color = "green";
+        this.style.fontSize = "35px";
+
+    })
 })
 
