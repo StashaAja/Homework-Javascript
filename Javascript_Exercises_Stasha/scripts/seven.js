93d2ed48ccb689e3b50e192374892467e126dc77
Stasha Damovska <75165799+StashaAja@users.noreply.github.com>
2021-01-26 22:18:59 +0100

    Update seven.js

--- Javascript_Exercises_Stasha/scripts/seven.js
+++ Javascript_Exercises_Stasha/scripts/seven.js
@@ -1,14 +1,34 @@
-let petsArray = ["Aja the dog","Pepe the dog","Buf the owl","Coco the parot","Baby the cat","Lucifer the Cat"];
+let userButton = document.getElementById("userButton");
+let circleDiv = document.getElementsByTagName("div")[1];
 
-let folksArrey = ["Stasha","Elena","Zorichka","Renaka","Neno"];
-
-let inputUser = document.getElementById("inputUser").value;
-let buttonUser = document.getElementById("buttonUser");
-
-
-function Person (FName,Pet){
-    this.petsArray = FName;
-    this.folksArrey = Pet;
-    this.userInput = inputUser;
-    
+function Circle (rad){
+    this.radius = rad;
+    this.pi = 3,14;
+    this.getArea = function(){
+        let area = (this.radius*this.radius)*this.pi
+        return area;
+    }
+    this.getPerimeter = function(){
+        let perimeter = 2*this.radius*this.pi;
+        return perimeter;
+    }
 }
+
+//PRASHANJE:
+// userButton.addEventListener("click",function(){
+// let circleUser = new Circle (userRadius);  // Question: Zashto tuka koga go zadavam user Radius nekje?
+// console.log(circleUser.getArea());
+// console.log(circleUser.getPerimeter());
+// })
+
+
+userButton.addEventListener("click",function(){
+    let userRadius = document.getElementById("userRadius").value;
+    let circleUser = new Circle (userRadius);
+    let inputArea = document.createElement("input");
+    inputArea.value=`This is the area: ${circleUser.getArea()}` ;
+    circleDiv.appendChild(inputArea);
+    let inputPerimeter = document.createElement("input");
+    inputPerimeter.value=`This is the perimeter: ${circleUser.getPerimeter()}` ;
+    circleDiv.appendChild(inputPerimeter);
+    })
