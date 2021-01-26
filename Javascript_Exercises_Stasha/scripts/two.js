dec48b83a8a9e5a770d76b066d8972c3f39ad35e
Stasha Damovska <75165799+StashaAja@users.noreply.github.com>
2021-01-26 12:37:53 +0100

    Update two.js

--- Javascript_Exercises_Stasha/scripts/two.js
+++ Javascript_Exercises_Stasha/scripts/two.js
@@ -1,29 +1,34 @@
 let brojche = parseInt(prompt(`Please enter a number:`));
 
-function Number (brojche){
-
-    this.Number = brojche;
-
-    this.numberFunctionPsoitive = function(){
-        if(this.Number % 2){
-            alert(`the number is even`);
-        } else {
-            alert(`the number is odd`);
-        }
+function checkNumberIsPositive(brojche) {
+    if (brojche >= 0) {
+        alert(`The number ${brojche} is a positive number!`)
+    } else if (brojche < 0) {
+        alert(`The number ${brojche} is a negative number!`)
     }
-
-    this.numberFunctionNumberofDigits() = function(){
-        if(this.Number >= 0){
-            alert(`the number is positive`)
-        } else{
-            alert(`the number is not positive`)
-        }
-        
-    }
-
 }
 
-let numberOne = new Number(brojche);
+checkNumberIsPositive(brojche);
+
+function checkNumberDigits(brojche) {
+
+    let digits = brojche.toString().length;
+    alert(`Your number  has a total of ${digits} digits!`)
+    return digits;
+}
+
+checkNumberDigits(brojche);
+
+function checkEvenOdd(brojche) {
+
+    if (brojche % 2 == 0) {
+        alert(`The number ${brojche} is even!`)
+    } else (
+        alert(`The number ${brojche} is odd!`)
+    )
+}
+
+checkEvenOdd(brojche);
 
 
 
