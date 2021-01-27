2a4f061f7385c5989715100aadde081ca9b2b967
Stasha Damovska <75165799+StashaAja@users.noreply.github.com>
2021-01-27 11:03:00 +0100

    Update eigth.js

--- Javascript_Exercises_Stasha/scripts/eigth.js
+++ Javascript_Exercises_Stasha/scripts/eigth.js
@@ -1,1 +1,15 @@
+let fixedArray = [{ fullName: "Asja Zabijakin", jobTitle: "writer", salary: 3500 }, { fullName: "Jan Ahrimenko", jobTitle: "dancer", salary: 4000 }];
 
+function getSalariesFromPeople(array) {
+    let sumOfSalaries = 0;
+    for (a of array) {
+        // console.log(a); //object
+        //  console.log(a.salary);
+        //  console.log(typeof(a.salary));//number
+
+        sumOfSalaries += a.salary;
+    }
+    return sumOfSalaries;
+}
+
+console.log(`This is the sum of the salaries: ${getSalariesFromPeople(fixedArray)}`);
